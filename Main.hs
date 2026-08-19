3fca3b5191fa25186438fc9b184e01acda3e3331
Kiju <dev@jakubkijek.com>
2026-08-19 14:44:30 +0200

    feat: implement functor and applicative for the parser

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.Char
+
 data JsonValue
   = JsonNull
   | JsonBool
@@ -13,14 +15,33 @@ newtype Parser a = Parser
   { runParser :: String -> Maybe (String, a)
   }
 
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \input -> case p input of
+    Nothing -> Nothing
+    Just (rest, x) -> Just (rest, f x)
+
+instance Applicative Parser where
+  pure x = Parser $ \input -> Just (input, x)
+  (Parser p1) <*> (Parser p2) = Parser $ \input ->
+    case p1 input of
+      Nothing -> Nothing
+      Just (r1, f) -> case p2 r1 of
+        Nothing -> Nothing
+        Just (r2, x) -> Just (r2, f x)
+
 jsonNull :: Parser JsonValue
 jsonNull = undefined
 
 charP :: Char -> Parser Char
-charP x = Parser $ \input ->
-  case input of
-    y : ys | y == x -> Just (ys, x)
-    _ -> Nothing
+charP x = Parser f
+  where
+    f (y : ys)
+      | y == x = Just (ys, x)
+      | otherwise = Nothing
+    f [] = Nothing
+
+-- stringP :: String -> Parser String
+-- stringP = sequenceA . map charP
 
 main :: IO ()
 main = undefined
